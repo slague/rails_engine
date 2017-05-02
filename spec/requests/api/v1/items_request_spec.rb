f1f083b270dd3047f2382d27d786da3139cfff58
Sam Landfried <samlandfried@gmail.com>
2017-05-02 15:07:23 -0600

    Add items show page

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -15,10 +15,31 @@ describe 'Item API' do
       expect(response).to be_success
 
       items = JSON.parse(response.body)
+
       expect(items.first['name']).to eq(item1.name)
       expect(items.first['description']).to eq(item1.description)
+      expect(items.first['unit_price']).to eq(item1.unit_price)
       expect(items.first).to_not have_key 'created_at'
       expect(items.first).to_not have_key 'updated_at'
+
+      expect(items.last['name']).to eq(item2.name)
+      expect(items.last['description']).to eq(item2.description)
+      expect(items.last['unit_price']).to eq(item2.unit_price)
+      expect(items.last).to_not have_key 'created_at'
+      expect(items.last).to_not have_key 'updated_at'
+    end
+
+    it 'returns one item' do
+      get "/api/v1/items/#{item1.id}.json"
+
+      expect(response).to be_success
+
+      item = JSON.parse(response.body)
+
+      expect(item['name']).to eq(item1.name)
+      expect(item['description']).to eq(item1.description)
+
+      expect(response.body).to_not include item2.name
     end
   end
 end
