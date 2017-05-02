47900d4490198759dd6d016b3ca81f0e2d655601
Sam Landfried <samlandfried@gmail.com>
2017-05-01 20:24:39 -0600

    Build merchants index api

--- spec/factories.rb
+++ spec/factories.rb
@@ -4,5 +4,8 @@ FactoryGirl.define do
     sequence(:name) do |n|
       "Merchant ##{n}"
     end
+
+    created_at DateTime.new(2017,5,1,20,13,20)
+    updated_at DateTime.new(2017,5,1,20,13,20)
   end
-end 
+end
--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -4,7 +4,7 @@ describe 'Merchants API' do
   it 'sends all merchants' do
     create_list(:merchant, 2)
 
-    get 'api/v1/merchants.json'
+    get '/api/v1/merchants.json'
 
     expect(response).to be_success
 
@@ -13,7 +13,7 @@ describe 'Merchants API' do
 
     expect(merchants.count).to eq 2
     expect(merchant['name']).to eq 'Merchant #1'
-    expect(merchant['created_at']).to be DateTime
-    expect(merchant['updated_at']).to be DateTime
+    expect(merchant['created_at']).to eq '2017-05-01T20:13:20.000Z'
+    expect(merchant['updated_at']).to eq '2017-05-01T20:13:20.000Z'
   end
 end
