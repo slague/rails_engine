09a1bbffe07929f2d75e9a865faeabc9c0ac948f
Sam Landfried <samlandfried@gmail.com>
2017-05-02 14:13:38 -0600

    Test and implement merchant

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,2 +1,6 @@
 class Merchant < ActiveRecord::Base
+  has_many :items
+
+  validates :name, :created_at, :updated_at, presence: true
+  validates_uniqueness_of :name
 end
---
+++ spec/models/merchant_spec.rb
@@ -0,0 +1,51 @@
+require 'rails_helper'
+
+RSpec.describe Merchant do
+    attr_reader :time
+    before do
+      @time = DateTime.new(2017,5,1,20,13,20)
+    end
+  context 'validations' do
+    it 'is valid with a name, created_at, and updated_at' do
+      merchant = Merchant.new(name: "seller person", created_at: time, updated_at: time)
+
+      expect(merchant).to be_valid
+    end
+    it 'is invalid without a name' do
+      merchant = Merchant.new(created_at: time, updated_at: time)
+
+      expect(merchant).to_not be_valid
+    end
+
+    it 'is invalid without created_at' do
+      merchant = Merchant.new(name: "seller person", updated_at: time)
+
+      expect(merchant).to_not be_valid
+    end
+
+    it 'is invalid without updated_at' do
+      merchant = Merchant.new(name: "seller person", created_at: time)
+
+      expect(merchant).to_not be_valid
+    end
+
+    it 'is invalid with a duplicate name' do
+      Merchant.create(name: "seller person", created_at: time, updated_at: time)
+      merchant = Merchant.new(name: "seller person", created_at: time, updated_at: time)
+
+      expect(merchant).to_not be_valid
+    end
+  end
+
+  context 'relationships' do
+    it 'has many items' do
+      items = create_list(:item, 2)
+      merchant = Merchant.create(name: "seller person", created_at: time, updated_at: time)
+      merchant.items << items
+
+      expect(merchant.items).to include(items.first)
+      expect(merchant.items).to include(items.last)
+
+    end
+  end
+end
