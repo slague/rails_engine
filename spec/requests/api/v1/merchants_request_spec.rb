2e800df17cde46c0cac7c6a191a306130353f092
Sam Landfried <samlandfried@gmail.com>
2017-05-02 13:17:11 -0600

    Add context blocks to merchants request spec

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -1,34 +1,45 @@
 require 'rails_helper'
 
 describe 'Merchants API' do
-  it 'sends all merchants' do
-    create_list(:merchant, 2)
+  context 'record end points' do
+	  it 'sends all merchants' do
+	    create_list(:merchant, 2)
 
-    get '/api/v1/merchants.json'
+	    get '/api/v1/merchants.json'
 
-    expect(response).to be_success
+	    expect(response).to be_success
 
-    merchants = JSON.parse response.body
-    merchant = merchants.first
+	    merchants = JSON.parse response.body
+	    merchant = merchants.first
 
-    expect(merchants.count).to eq 2
-    expect(merchant['name']).to eq 'Merchant #1'
-    expect(merchant).to_not have_key 'created_at'
-    expect(merchant).to_not have_key 'updated_at'
+	    expect(merchants.count).to eq 2
+	    expect(merchant['name']).to eq 'Merchant #1'
+	    expect(merchant).to_not have_key 'created_at'
+	    expect(merchant).to_not have_key 'updated_at'
+	  end
+
+	  it 'can show one merchant' do
+	    merchant = create :merchant
+
+	    get "/api/v1/merchants/#{merchant.id}.json"
+
+	    expect(response).to be_success
+
+	    response_merchant = JSON.parse response.body
+
+	    expect(response_merchant['id']).to eq merchant.id
+	    expect(response_merchant['name']).to eq merchant.name
+	    expect(response_merchant).to_not have_key 'created_at'
+	    expect(response_merchant).to_not have_key 'updated_at'
+	  end
   end
 
-  it 'can show one merchant' do
-    merchant = create :merchant
+  context 'business intelligence end points' do
+    it 'returns the top x merchants ranked by total revenue' do
 
-    get "/api/v1/merchants/#{merchant.id}.json"
 
-    expect(response).to be_success
+    end
 
-    response_merchant = JSON.parse response.body
-
-    expect(response_merchant['id']).to eq merchant.id
-    expect(response_merchant['name']).to eq merchant.name
-    expect(response_merchant).to_not have_key 'created_at'
-    expect(response_merchant).to_not have_key 'updated_at'
   end
+
 end
