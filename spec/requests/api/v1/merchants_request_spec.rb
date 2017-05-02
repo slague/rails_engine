c3ecef400ce051dc851da5838247e244a67a4d54
Sam Landfried <samlandfried@gmail.com>
2017-05-02 11:03:08 -0600

    Build merchant show API and test

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -13,7 +13,22 @@ describe 'Merchants API' do
 
     expect(merchants.count).to eq 2
     expect(merchant['name']).to eq 'Merchant #1'
-    expect(merchant['created_at']).to eq '2017-05-01T20:13:20.000Z'
-    expect(merchant['updated_at']).to eq '2017-05-01T20:13:20.000Z'
+    expect(merchant).to_not have_key 'created_at'
+    expect(merchant).to_not have_key 'updated_at'
+  end
+
+  it 'can show one merchant' do
+    merchant = create :merchant
+
+    get "/api/v1/merchants/#{merchant.id}.json"
+
+    expect(response).to be_success
+
+    response_merchant = JSON.parse response.body
+
+    expect(response_merchant['id']).to eq merchant.id
+    expect(response_merchant['name']).to eq merchant.name
+    expect(response_merchant).to_not have_key 'created_at'
+    expect(response_merchant).to_not have_key 'updated_at'
   end
 end
