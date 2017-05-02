86699ec6ee9c998df9464a5847b4c97bce7660f1
Sam Landfried <samlandfried@gmail.com>
2017-05-02 17:40:55 -0600

    Test invoices record end points
    Fix hound issues
    Fix more Hound issues
    Fix more more Hound issues
    Fix more more more Hound issues

--- spec/factories.rb
+++ spec/factories.rb
@@ -5,18 +5,18 @@ FactoryGirl.define do
       "Merchant ##{n}"
     end
 
-    created_at DateTime.new(2017,5,1,20,13,20)
-    updated_at DateTime.new(2017,5,1,20,13,20)
+    created_at DateTime.new(2017, 5, 1, 20, 13, 20)
+    updated_at DateTime.new(2017, 5, 1, 20, 13, 20)
   end
 
   factory :item do
     sequence(:name) do |n|
       "Item ##{n}"
     end
-    description "Item description"
+    description 'Item description'
     unit_price  1000
     merchant
-    created_at DateTime.new(2017,5,1,20,13,20)
-    updated_at DateTime.new(2017,5,1,20,13,20)
+    created_at DateTime.new(2017, 5, 1, 20, 13, 20)
+    updated_at DateTime.new(2017, 5, 1, 20, 13, 20)
   end
 end
